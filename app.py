f2d833f7aef9c2032d4d0ec70f8f027f483e3a48
LUCAS BARBAROTO MIOTTO <22.01388-0@maua.br>
2023-05-06 17:26:48 -0300

    Projeto pronto

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+import chatgpt
+import tela
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+def criar_pergunta(assunto, tipo, dificuldade, pergunta_exemplo):
+    return chatgpt.criar_pergunta(OPENAI_API_KEY, assunto, tipo, dificuldade, pergunta_exemplo)
+
+def responder_pergunta (pergunta_a_ser_respondida):
+    return chatgpt.responder_pergunta(OPENAI_API_KEY, pergunta_a_ser_respondida)
+
+
+tela.criar_tela(criar_pergunta, responder_pergunta)
